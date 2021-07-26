3656cb2407fece616eb844f95bc1516c4e18774e
adrianhosman <48504784+adrianhosman@users.noreply.github.com>
2021-07-26 15:27:40 +0700

    Add files via upload

---
+++ handler/invoice.go
@@ -0,0 +1,36 @@
+package handler
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/fgunawan1995/xendit/model"
+	"github.com/gorilla/mux"
+	"github.com/pkg/errors"
+)
+
+func writeErrorResponse(w http.ResponseWriter, statusCode int, err error) {
+	w.WriteHeader(statusCode)
+	json.NewEncoder(w).Encode(model.BuildAPIResponseError(statusCode, errors.Cause(err)))
+}
+
+func writeSuccessResponse(w http.ResponseWriter, data interface{}) {
+	statusCode := http.StatusOK
+	w.WriteHeader(statusCode)
+	json.NewEncoder(w).Encode(model.BuildAPIResponseSuccess(statusCode, data))
+}
+
+func (h *Handler) CalculateInvoice(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	businessID := vars["business_id"]
+	if businessID == "" {
+		writeErrorResponse(w, http.StatusBadRequest, errors.New("business_id must provided"))
+		return
+	}
+	result, err := h.usecase.CalculateInvoiceData(businessID)
+	if err != nil {
+		writeErrorResponse(w, http.StatusInternalServerError, err)
+		return
+	}
+	writeSuccessResponse(w, result)
+}
---
+++ model/invoice.go
@@ -0,0 +1,16 @@
+package model
+
+type CalculationInvoiceResponse struct {
+	BusinessID             string            `json:"business_id"`
+	SubtotalPrice          float64           `json:"subtotal"`
+	SubTotalPriceFormatted string            `json:"subtotal_price_formatted"`
+	DetailProduct          DetailProductData `json:"detail_product"`
+}
+
+type DetailProductData struct {
+	Name                  string `json:"name"`
+	UnitPriceFormatted    string `json:"unit_price_formatted"`
+	Qty                   int    `json:"qty"`
+	VolumeFormatted       string `json:"volume_formatted"`
+	BilledAmountFormatted string `json:"billed_amount_formatted"`
+}
